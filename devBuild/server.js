731a797e59237a7662cbd41bbdbe77c00156f4cb
unknown <ahamoodi178@gmail.com>
2020-01-07 15:13:57 -0500

    Deployment 2

--- devBuild/server.js
+++ devBuild/server.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
 const server1 = require("./app.js");
-const server2 = require("./socketserver.js");
 
-server1.listen(2000);
+server1.listen(process.env.PORT || 2000);
 console.log("[SERVER]: started");
-
-server2.listen(82);
-console.log("[SOCKET-SERVER]: started");
--- devBuild/socketserver.js
+++ devBuild/socketserver.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const server = require("http").Server(app);
+const server = require("./app.js");
 const io = require("socket.io")(server);
 
 app.get("/", function(req, res) {

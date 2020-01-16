bc8feb3cbdfc620f4646f6a9321ce0ea12b406f4
mero789 <ahamoodi178@gmail.com>
2020-01-16 12:59:06 -0500

    Add files via upload

--- schoolhelp.sql
+++ schoolhelp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2019 at 04:32 AM
+-- Generation Time: Jan 16, 2020 at 06:57 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
@@ -55,7 +55,9 @@ INSERT INTO `answers` (`id`, `title`, `description`, `madeBy`) VALUES
 (0, 'e', 'e', ''),
 (0, 'a', 'a', ''),
 (39, 're', 're', ''),
-(39, 'a', 'a', '');
+(39, 'a', 'a', ''),
+(39, 'ok', 'so lets see if it works', 'uwu'),
+(39, 'Okay so this is a test', 'ok', 'uwu');
 
 -- --------------------------------------------------------
 
@@ -68,7 +70,7 @@ CREATE TABLE `posts` (
   `madeBy` varchar(255) NOT NULL,
   `question` varchar(255) NOT NULL,
   `description` longtext NOT NULL,
-  `answer` longtext NOT NULL,
+  `answer` int(11) NOT NULL,
   `upvotes` bigint(20) NOT NULL DEFAULT '0',
   `downvotes` bigint(20) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -78,10 +80,38 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `madeBy`, `question`, `description`, `answer`, `upvotes`, `downvotes`) VALUES
-(32, 'uwu', 'Testing the vote system', 'Ok so im basically testing our vote system', '', 1, 0),
-(37, 'uwu', 'REEEEEEEE', 'REEEEEEEEEEEEEEEEEEEEEEEEEEEE', '', 0, 0),
-(38, 'uwu', 'aa', 'aa', '', 0, 0),
-(39, 'uwu', 'a', 'a', '', 0, 1);
+(32, 'uwu', 'Testing the vote system', 'Ok so im basically testing our vote system', 0, 2, 0),
+(37, 'uwu', 'REEEEEEEE', 'REEEEEEEEEEEEEEEEEEEEEEEEEEEE', 0, 1, 0),
+(38, 'uwu', 'aa', 'aa', 0, 1, 0),
+(39, 'uwu', 'a', 'a', 0, 1, 1),
+(40, 'mero', 'test', '        aaaaa', 0, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiles`
+--
+
+CREATE TABLE `profiles` (
+  `username` varchar(255) NOT NULL,
+  `pfp` varchar(255) NOT NULL,
+  `descrip` varchar(255) NOT NULL,
+  `board` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `profiles`
+--
+
+INSERT INTO `profiles` (`username`, `pfp`, `descrip`, `board`) VALUES
+('Ameer Hamoodi', 'profileplaceholder_1', 'This user likes to keep a sense of mystery in the air ...', 'hwdsb'),
+('Mero789', 'profileplaceholder_7', 'This user likes to keep a sense of mystery in the air ...', 'gmail'),
+('bob', 'profileplaceholder_6', 'This user likes to keep a sense of mystery in the air ...', 'gmail'),
+('mero', 'profileplaceholder_4', 'This user likes to keep a sense of mystery in the air ...', 'gmail'),
+('mero', 'profileplaceholder_5', 'This user likes to keep a sense of mystery in the air ...', 'gmail'),
+('mero', 'profileplaceholder_1', 'This user likes to keep a sense of mystery in the air ...', 'gmail'),
+('mero', 'profileplaceholder_2', 'This user likes to keep a sense of mystery in the air ...', 'gmail'),
+('ameer hamoodi', 'profileplaceholder_7', 'This user likes to keep a sense of mystery in the air ...', 'hwdsb');
 
 -- --------------------------------------------------------
 
@@ -93,21 +123,18 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `verified` varchar(12) NOT NULL DEFAULT 'no',
+  `vid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(10, 'qqqq', '', 'qqqq'),
-(11, 'Ameer Hamoodi', '', 'pricey31'),
-(12, 'anflansnasl', '', 'asfaf'),
-(13, 'Mero Hamo', '', 'pricey31'),
-(14, 'www', '', 'pricey31'),
-(15, 'uwu', 'ahamoodi178@gmail.com', 'pricey31'),
-(16, 'mero789y', 'chiplover789@gmail.com', 'pricey31');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `verified`, `vid`) VALUES
+(24, 'mero', 'ahamoodi178@gmail.com', 'pricey31', 'no', 424449),
+(25, 'ameer hamoodi', 'ahamoodi0602@hwdsb.on.ca', 'pricey31', 'no', 344344);
 
 --
 -- Indexes for dumped tables
@@ -133,13 +160,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
